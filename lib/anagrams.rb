08ac12e7c27664576937d355a54c9afa5e7d690c
akane0915 <asialkane@gmail.com>
2017-04-21 10:29:04 -0700

    create method is_a_word? to call to_lower_case method and return true if all words contain a vowel or return false if any words have no vowels

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,4 +2,25 @@ class String
   define_method(:to_lower_case) do
     lower_case_string = self.downcase()
   end
-end
+
+  define_method(:is_a_word?) do
+    array_of_words = self.to_lower_case.split(" ")
+    new_array = []
+    array_of_words.each() do |word|
+      if word.include?("a") | word.include?("e") | word.include?("i") | word.include?("o") | word.include?("u") | word.include?("y")
+        new_array.push(word)
+      else
+        new_array.push("array contains string that is not a word")
+      end
+    end
+    new_array
+    unless new_array.include?("array contains string that is not a word")
+      true
+    else
+      false
+    end #End unless statement
+  end #End method "is_a_word?"
+
+
+
+end #End class String
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,7 +2,19 @@ require ('rspec')
 require ('anagrams.rb')
 
 describe('String#to_lower_case') do
-  it('takes user input string and downcases all letters') do
+  it('takes input string and downcases all letters') do
     expect(("A").to_lower_case()).to(eq("a"))
+  end
+end
+
+describe('String#is_a_word?') do
+  it('takes input string and determines if all words contain a vowel') do
+    expect(("bc").is_a_word?()).to(eq(false))
+  end
+end
+
+describe('String#is_a_word?') do
+  it('takes input string and determines if all words contain a vowel') do
+    expect(("bA").is_a_word?()).to(eq(true))
   end
 end
